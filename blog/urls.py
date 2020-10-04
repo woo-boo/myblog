ba5268ff7c9722f98ef2529ac9eeda65fb2e814a
Andrey <andreybyrk@gmail.com>
2020-10-04 23:34:46 +0300

    change views to CBV

--- blog/urls.py
+++ blog/urls.py
@@ -1,51 +1,55 @@
 from django.urls import path, include
 from django.views.generic import RedirectView
-from . import views
+from blog import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='home/', permanent=True)),
     
-    path('home/', views.home, name="home"),
+    path('home/', views.HomeView.as_view(), name="home"),
 
-    path('me/', views.my_account, name="my_account"),
+    path('me/', views.MyAccountView.as_view(), name="my_account"),
 
-    path('me/blogs/', views.my_blogs, name="my_blogs"),
+    path('me/edit_profile/', views.MyAccountEditView.as_view(), name="my_account_edit"),
 
-    path('me/posts/', views.my_posts, name="my_posts"),
+    path('me/blogs/', views.MyBlogsView.as_view(), name="my_blogs"),
 
-    path('subscriptions/', views.subscriptions, name="subscriptions"),
+    path('me/posts/', views.MyPostsView.as_view(), name="my_posts"),
 
-    path('new_blog/', views.blog_new, name="blog_new"),
+    path('me/subscriptions/', views.MySubsriptionsView.as_view(), name="subscriptions"),
+
+    path('new_blog/', views.BlogNewView.as_view(), name="blog_new"),
     
-    path('new_post/', views.select_blog_to_add_post, name="select_blog_to_add_post"),
+    path('new_post/', views.BlogAddPostView.as_view(), name="select_blog_to_add_post"),
 
-    path('news/', views.post_list, name="post_list"),
+    path('news/', views.PostListView.as_view(), name="post_list"),
 
-    path('news/page-<int:page>/', views.post_list, name="post_list_page"),
+    path('news/page-<int:page>/', views.PostListView.as_view(), name="post_list_page"),
 
-    path('blogs/', views.blog_list, name="blog_list"),
+    path('blogs/', views.BlogListView.as_view(), name="blog_list"),
 
-    path('blogs/page-<int:page>/', views.blog_list, name="blog_list_page"),
+    path('blogs/page-<int:page>/', views.BlogListView.as_view(), name="blog_list_page"),
 
-    path('post/<int:post_id>/', views.post_detail, name="post_detail"),
+    path('post/<int:post_id>/', views.PostDetailView.as_view(), name="post_detail"),
 
-    path('post/<int:post_id>/edit/', views.post_edit, name="post_edit"),
+    path('post/<int:post_id>/edit/', views.PostEditView.as_view(), name="post_edit"),
 
-    path('post/<int:post_id>/delete/', views.post_delete, name="post_delete"),
+    path('post/<int:post_id>/delete/', views.PostDeleteView.as_view(), name="post_delete"),
 
-    path('blog/<int:blog_id>/', views.blog_detail, name="blog_detail"),
+    path('blog/<int:blog_id>/', views.BlogDetailView.as_view(), name="blog_detail"),
 
-    path('blog/<int:blog_id>/new_post/', views.post_new, name="post_new"),
+    path('blog/<int:blog_id>/new_post/', views.PostNewView.as_view(), name="post_new"),
 
-    path('blog/<int:blog_id>/delete', views.blog_delete, name="blog_delete"),
+    path('blog/<int:blog_id>/delete', views.BlogDeleteView.as_view(), name="blog_delete"),
 
-    path('blog/<int:blog_id>/page-<int:page>/', views.blog_detail, name="blog_detail_page"),
+    path('blog/<int:blog_id>/page-<int:page>/', views.BlogDetailView.as_view(), name="blog_detail_page"),
 
-    path('users/', views.user_list, name="user_list"),
+    path('users/', views.UserListView.as_view(), name="user_list"),
 
-    path('user/<int:user_id>/', views.user_detail, name="user_detail"),
+    path('user/<int:user_id>/', views.UserDetailView.as_view(), name="user_detail"),
 
-    path('login/', views.log_in, name="log_in"),
+    path('login/', views.LogInView.as_view(), name="log_in"),
 
-    path('logout/', views.log_out, name="log_out"),
+    path('logout/', views.LogOutView.as_view(), name="log_out"),
+
+    path('sign_up/', views.SignUpView.as_view(), name="sign_up"),
 ]
